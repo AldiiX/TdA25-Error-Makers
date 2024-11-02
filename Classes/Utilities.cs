bbe39a3f739959715649fb2d0547949c342220c5
AldiiX <aldiix@email.cz>
2024-11-02 20:38:26 +0100

    FEAT: ,,design" pro hello page
    - pridany i obrazky (loga atd)
    - pridan dark/light mode
    - pozdeji pouzit hello page jako error page

---
+++ Classes/Utilities.cs
@@ -0,0 +1,14 @@
+﻿namespace TdA25_Error_Makers.Classes;
+
+public class Utilities {
+
+
+    public static class WebTheme {
+        public static string Get() {
+            // TODO: Implementovat zjištění tématu z cookies
+            return "light";
+        }
+
+        public static string GetCSSPath() => $"/css/themes/{Get().ToLower()}.css";
+    }
+}
