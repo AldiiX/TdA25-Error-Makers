e232a2515400cf59b622df23a94195a2f038dc42
DG_Kuba <jakubfunk90@gmail.com>
2024-12-10 20:42:26 +0100

    FEAT: LOGIN

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿namespace TdA25_Error_Makers.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+public class LoginController : Controller {
+    [Route("/login")]
+    public IActionResult Login_Page() => View("/Views/Login.cshtml");
+    
+    [Route("/login")]
+    [HttpPost]
+    public IActionResult Login_Post(string username, string password) {
+        // This is where you would check the user's credentials
+        // and log them in if they are correct.
+        return Redirect("/");
+    }
+    
+}
