d5d6c95c7b33ee23879ed06afb7f09b934f3271e
Wedralda <pavel.andrle@educhem.cz>
2025-01-12 03:00:07 +0100

    FEAT: Acc page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TdA25_Error_Makers.Controllers;
+
+public class AccountController : Controller {
+
+    [HttpGet("/account")]
+    public IActionResult Games() => View("/Views/Account.cshtml");
+
+    [HttpGet("/ucet")]
+    public IActionResult Redirection() => Redirect("/games");
+}
