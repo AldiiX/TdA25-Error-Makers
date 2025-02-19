dd5bf7d8377b10ffa9fb07dafb3ce913edaa8dfa
AldiiX <aldiix@email.cz>
2025-02-19 20:43:53 +0100

    FEAT: příprava pro leaderboard page

---
+++ Controllers/LeaderboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TdA25_Error_Makers.Controllers;
+
+public class LeaderboardController : Controller {
+
+    [HttpGet("/leaderboard")]
+    public IActionResult Leaderboard() {
+        return View("/Views/Leaderboard.cshtml");
+    }
+}
