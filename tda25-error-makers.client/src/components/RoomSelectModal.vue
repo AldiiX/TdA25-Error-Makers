9e0c6d5959b49a840538ec0c2b7a847b2585a4a0
DG_Kuba <jakubfunk90@gmail.com>
2025-03-22 13:02:28 +0100

    FEAT: RoomModal

--- tda25-error-makers.client/src/components/RoomSelectModal.vue
+++ tda25-error-makers.client/src/components/RoomSelectModal.vue
@@ -13,7 +13,7 @@ const props = defineProps<{
             <div class="inputs">
                 <label for="kod">Zadej kód místnosti</label>
                 <div class="input-group">
-                    <input name="kod" type="text" maxlength="6" placeholder="Zadejte kód místnosti"/>
+                    <input name="kod" type="text" maxlength="6" placeholder="000000"/>
                     <button class="button-primary-col-sec">Připojit se</button>
                 </div>
             </div>
@@ -22,41 +22,6 @@ const props = defineProps<{
     </div>
 </template>
 
-<style scoped>
-.modal-overlay {
-    position: fixed;
-    backdrop-filter: blur(10px) brightness(90%);
-    width: 100%;
-    height: 100%;
-    z-index: 1;
+<style scoped src="./RoomSelectModal.scss">
 
-    >.modal-body {
-        position: fixed;
-        width: 320px;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        z-index: 2;
-        background-color: white;
-        padding: 48px;
-        border-radius: 8px;
-
-        >.inputs{
-            >label{
-                font-size: 24px;
-                font-weight: 600;
-                color: var(--text-color);
-            }
-
-            >.input-group{
-                display: flex;
-               justify-content: space-between;
-
-                >input{
-                    width: 45%;
-                }
-            }
-        }
-    }
-}
 </style>
