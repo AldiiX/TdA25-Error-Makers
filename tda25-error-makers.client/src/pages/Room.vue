6db6c456652fb99a186d641a854e2e05f8276dcd
Pavel Andrle <pavel.andrle@educhem.cz>
2025-03-22 14:51:29 +0100

    FEAT: user count

--- tda25-error-makers.client/src/pages/Room.vue
+++ tda25-error-makers.client/src/pages/Room.vue
@@ -7,6 +7,7 @@
 
     import BlurBackground from "@/components/backgrounds/BlurBackground.vue";
     import {inject, onMounted, ref, type Ref} from "vue";
+    import Room from "@/pages/Room.vue";
     const loggedUser = inject("loggedUser") as Ref<any>;
     let socket: WebSocket | null = null;
     const room = ref<any | null>(null);
@@ -44,14 +45,20 @@
 
     <div class="sections">
         <div class="mainsection">
-                <p>Prezentující</p>
+            <div class="top">
+                <p class="text">Prezentující</p>
+                <div class="usercount">
+                    <div class="icon"></div>
+                    <p>{{room?.connectedUsers.length}}</p>
+                </div>
+            </div>
             <div class="User" v-for="user in room.connectedUsers" v-if="room?.connectedUsers">
               <template v-if="user.isPresenter">
                 <p class="title">{{ user.name[0].toUpperCase() }}</p>
                 <p class="username">{{ user.name }}</p>
               </template>
             </div>
-            <p>Účastníci</p>
+            <p class="text">Účastníci</p>
 
             <div class="users">
                 <div class="User" v-for="user in  room.connectedUsers" v-if="room?.connectedUsers">
